b738699963621968183c16ca67efd6d79a302046
ZakPhotographer <karouali12@gmail.com>
2024-09-12 12:34:35 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -182,15 +182,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # E-mail configuration
 
 EMAIL_BACKEND = config(
-    "EMAIL_BACKEND", default="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
 )
-EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
-EMAIL_PORT = config("EMAIL_PORT", default=587)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_FROM_ADDRESS = config("EMAIL_FROM_ADDRESS")
-EMAIL_USE_SSL = False
+EMAIL_HOST=smtp.gmail.com
+EMAIL_PORT=587
+EMAIL_USE_TLS=True
+EMAIL_FROM_ADDRESS=rochdi.karouali1234@gmail.com
+EMAIL_HOST_USER=Zakariarahimi1998@gmail.com
+EMAIL_HOST_PASSWORD=Beeqcm@2024
+
 
 # crispy config
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
