a24f14e4e5578868d8b17152201576310abcd9ea
ZakPhotographer <karouali12@gmail.com>
2024-09-12 12:23:05 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from decouple import config
+EMAIL_HOST_USER=Zakariarahimi1998@gmail.com
+EMAIL_HOST_PASSWORD=Beeqcm@2024
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
